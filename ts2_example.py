f7782f500d2befd55d3a3abc74b846f6bc5fd16f
Kirsten Richardson (PGR) <b3024896@18scompl028.ncl.ac.uk>
2023-02-09 16:40:02 +0000

    delete example files as covered in readme now

--- ts2_example.py
+++
@@ -1,8 +0,0 @@
-from synthetic_data_metrics import evaluator
-from synthetic_data_metrics.datasets import load_timeseries
-
-real, synth = load_timeseries()
-ev = evaluator.TS_Evaluator(real, synth, target='ACTIVITY')
-print(ev.discriminative_score())
-tsne = ev.t_SNE()
-tsne.show()
